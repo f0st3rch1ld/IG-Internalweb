d1527acb2676faadda2b1607917b43b8948970cd
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-08-03 07:51:21 -0600

    too many updates lol

--- wp-content/plugins/kanbanotron/db/qb_db/salesorder_price_calculation.php
+++ wp-content/plugins/kanbanotron/db/qb_db/salesorder_price_calculation.php
@@ -10,3 +10,5 @@ function calculate_order_total ($items_array) {
         $order_total = $order_total + (intval($items_array[$i]['Item_Price']) * intval($items_array[$i]['Item_Reorder_Amount']));
     }
 }
+
+echo 'Order Total: ' . $order_total . '; ';
