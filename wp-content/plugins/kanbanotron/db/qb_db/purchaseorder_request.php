65d826673ca4f6eb4ede6ce9bed2c484f0daa5de
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-08-04 10:58:21 -0600

    on_order_list

--- wp-content/plugins/kanbanotron/db/qb_db/purchaseorder_request.php
+++ wp-content/plugins/kanbanotron/db/qb_db/purchaseorder_request.php
@@ -3,13 +3,16 @@
 function retrieve_po_data()
 {
     $purchaseorder_table_data_array = array();
+    $purchaseorderlineret_table_data_array = array();
 
     // QuickBooks Database Connection
     include 'qb_data_connection.php';
 
+    // Request from purchaseorder table
     $purchaseorder_request = "SELECT TxnID, TimeCreated, VendorRef_FullName, Memo FROM purchaseorder WHERE IsManuallyClosed='0'";
     $purchaseorder_request_result = $conn->query($purchaseorder_request);
 
+    // Assigns request data to an array
     if ($purchaseorder_request_result->num_rows > 0) {
         while ($row = $purchaseorder_request_result->fetch_assoc()) {
             $temp_po_array = array(
@@ -22,7 +25,26 @@ function retrieve_po_data()
         }
     }
 
-    echo var_dump($purchaseorder_table_data_array);
+    // echo var_dump($purchaseorder_table_data_array);
+
+    // Request from purchaseorderlineret table
+    $purchaseorderlineret_request = "SELECT ItemRef_FullName, Description, Quantity, PARENT_IDKEY FROM purchaseorderlineret";
+    $purchaseorderlineret_request_result = $conn->query($purchaseorderlineret_request);
+
+    // Assigns request data to an array
+    if ($purchaseorder_request_result->num_rows > 0) {
+        while ($row = $purchaseorderlineret_request_result->fetch_assoc()) {
+            $temp_po_items_array = array(
+                'ItemRef_FullName' => $row['ItemRef_FullName'],
+                'Description' => $row['Description'],
+                'Quantity' => $row['Quantity'],
+                'PARENT_IDKEY' => $row['PARENT_IDKEY']
+            );
+            array_push($purchaseorderlineret_table_data_array, $temp_po_items_array);
+        }
+    }
+
+    echo var_dump($purchaseorderlineret_table_data_array);
 
     $conn->close();
 }
