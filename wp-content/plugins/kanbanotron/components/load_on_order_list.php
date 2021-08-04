0712274b06f56f49eaad84902cecb3941da5404d
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-08-04 13:51:00 -0600

    on_order_list

--- wp-content/plugins/kanbanotron/components/load_on_order_list.php
+++ wp-content/plugins/kanbanotron/components/load_on_order_list.php
@@ -17,56 +17,62 @@ retrieve_po_data();
 // ----Quantity
 // ----PARENT_IDKEY
 
-// vendors array holds which vendors have already been processed inside this loop
-$vendors = array();
+// order_txnid_array array holds which vendors have already been processed inside this loop
+$order_txnid_array = array();
 
 ?>
 
-<?php for ($i = 0; count($purchaseorder_table_data_array) > $i; $i++) : ?>
+<?php
+// Loops through all purchaseorder data, and groups / pushes vendors and vendor ids to array
+for ($i = 0; count($purchaseorder_table_data_array) > $i; $i++) {
+    if (!array_key_exists($purchaseorder_table_data_array[$i]['VendorRef_FullName'], $order_txnid_array)) {
+        $temp_array = array(
+            $purchaseorder_table_data_array[$i]['VendorRef_FullName'] => array(
+                $purchaseorder_table_data_array[$i]['TxnID']
+            )
+        );
+    } else {
+        array_push($order_txnid_array[$purchaseorder_table_data_array[$i]['VendorRef_FullName']], $purchaseorder_table_data_array[$i]['TxnID']);
+    }
+}
+?>
 
-    <?php if (!in_array($purchaseorder_table_data_array[$i]['VendorRef_FullName'], $vendors)) : ?>
-        <?php
-        array_push($vendors, $purchaseorder_table_data_array[$i]['VendorRef_FullName']);
-        ?>
-
-        <!-- Generated PO Table -->
-        <div class="purchase-order-container">
-            <div class="po-title-container">
-                <h5 class="vendor-name">
-                    <?php echo $purchaseorder_table_data_array[$i]['VendorRef_FullName']; ?>
-                </h5>
-            </div>
-            <table class="tablesorter">
-                <thead>
-                    <tr>
-                        <th>PN</th>
-                        <th>QTY</th>
-                        <th>Date Ordered</th>
-                        <th>PO</th>
-                        <th>ETA</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    <?php for ($y = 0; count($purchaseorderlineret_table_data_array) > $y; $y++) : ?>
-                        <?php if ($purchaseorderlineret_table_data_array[$y]['PARENT_IDKEY'] == $purchaseorder_table_data_array[$i]['TxnID']) : ?>
-                            <tr>
-                                <td class="tooltip"><?php echo $purchaseorderlineret_table_data_array[$y]['ItemRef_FullName']; ?>
-                                    <p class="tooltiptext"><?php echo $purchaseorderlineret_table_data_array[$y]['Description']; ?></p>
-                                </td>
-                                <td><?php echo $purchaseorderlineret_table_data_array[$y]['Quantity']; ?></td>
-                                <td><?php echo $purchaseorder_table_data_array[$i]['TimeCreated']; ?></td>
-                                <td><?php echo $purchaseorder_table_data_array[$i]['Memo']; ?></td>
-                                <td>ETA</td>
-                            </tr>
-                        <?php endif; ?>
-                    <?php endfor; ?>
-
-                </tbody>
-            </table>
+<?php for ($i = 0; count($order_txnid_array) > $i; $i++) : ?>
+    <!-- Generated PO Table -->
+    <div class="purchase-order-container">
+        <div class="po-title-container">
+            <h5 class="vendor-name">
+                <?php echo key($order_txnid_array[$i]); ?>
+            </h5>
         </div>
-        <!-- /Generated PO Table -->
+        <table class="tablesorter">
+            <thead>
+                <tr>
+                    <th>PN</th>
+                    <th>QTY</th>
+                    <th>Date Ordered</th>
+                    <th>PO</th>
+                    <th>ETA</th>
+                </tr>
+            </thead>
+            <tbody>
 
-    <?php endif; ?>
+                <?php for ($y = 0; count($purchaseorder_table_data_array) > $y; $y++) : ?>
+                    <?php if ($order_txnid_array[$i][$y] == $purchaseorder_table_data_array[$y]['TxnID']) : ?>
+                        <tr>
+                            <td class="tooltip"><?php echo $purchaseorderlineret_table_data_array[$y]['ItemRef_FullName']; ?>
+                                <p class="tooltiptext"><?php echo $purchaseorderlineret_table_data_array[$y]['Description']; ?></p>
+                            </td>
+                            <td><?php echo $purchaseorderlineret_table_data_array[$y]['Quantity']; ?></td>
+                            <td><?php echo $purchaseorder_table_data_array[$i]['TimeCreated']; ?></td>
+                            <td><?php echo $purchaseorder_table_data_array[$i]['Memo']; ?></td>
+                            <td>ETA</td>
+                        </tr>
+                    <?php endif; ?>
+                <?php endfor; ?>
 
+            </tbody>
+        </table>
+    </div>
+    <!-- /Generated PO Table -->
 <?php endfor; ?>
