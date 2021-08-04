cf16d054fb81c9f6770ae06e2bba186f5eaa17ce
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-08-04 10:00:41 -0600

    on_order_list

---
+++ wp-content/plugins/kanbanotron/components/load_on_order_list.php
@@ -0,0 +1,37 @@
+<!-- On Order List Container -->
+<div id="on-order-list-container">
+    <!-- Generated PO Table -->
+    <div class="purchase-order-container">
+        <div class="po-title-container">
+            <h5 class="vendor-name">Vendor</h5>
+        </div>
+        <table class="tablesorter">
+            <thead>
+                <tr>
+                    <th>PN</th>
+                    <th>Vendor PN</th>
+                    <th>QTY</th>
+                    <th>Date Ordered</th>
+                    <th>PO</th>
+                    <th>ETA</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="tooltip">PN
+                        <p class="tooltiptext">Description</p>
+                    </td>
+                    <td class="tooltip">Vendor PN
+                        <p class="tooltiptext">Description</p>
+                    </td>
+                    <td>QTY</td>
+                    <td>Date Ordered</td>
+                    <td>PO</td>
+                    <td>ETA</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <!-- /Generated PO Table -->
+</div>
+<!-- /On Order List Container -->
