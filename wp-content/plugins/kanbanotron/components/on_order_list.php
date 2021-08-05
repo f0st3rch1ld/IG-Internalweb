069b6388c6a263f3a9ab84cd37822dee8bee3437
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-08-05 09:08:10 -0600

    finishing touches on order overview

--- wp-content/plugins/kanbanotron/components/on_order_list.php
+++ wp-content/plugins/kanbanotron/components/on_order_list.php
@@ -12,6 +12,19 @@
 
 <!-- Page Refresh Script -->
 <script>
+    // AJAX request to refresh On-Order Overview
+    function refreshOverview() {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function() {
+            document.getElementById('on-order-list-container').innerHTML = this.responseText;
+        }
+        xhttp.open(
+            "GET",
+            `../../wp-content/plugins/kanbanotron/components/load_on_order_list.php/?xhttp=1`
+        );
+        xhttp.send();
+    }
+
     document.addEventListener('load', function() {
         setInterval(refreshOverview(), 5000);
     });
--- wp-content/plugins/kanbanotron/js/kanbanotron.js
+++ wp-content/plugins/kanbanotron/js/kanbanotron.js
@@ -134,19 +134,6 @@ function submitPurchaseOrder(x, y) {
 
 }
 
-// AJAX request to refresh On-Order Overview
-function refreshOverview() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function () {
-    document.getElementById('on-order-list-container').innerHTML = this.responseText;
-  }
-  xhttp.open(
-    "GET",
-    `../../wp-content/plugins/kanbanotron/components/load_on_order_list.php/?xhttp=1`
-  );
-  xhttp.send();
-}
-
 // Updates browser cookies to reflect chosen purchase order.
 function updateActiveOrder(x) {
   document.cookie = `working_purchase_order=${x}`;
