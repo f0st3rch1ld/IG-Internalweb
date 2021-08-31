2bcea586a0b18995b563fe94018046c4543c7c3c
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-08-31 09:08:58 -0600

    on order list request

--- wp-content/plugins/kanbanotron/components/load_on_order_list.php
+++ wp-content/plugins/kanbanotron/components/load_on_order_list.php
@@ -68,8 +68,15 @@ ksort($order_txnid_array);
                                 <td><?php echo $purchaseorderlineret_table_data_array[$y]['ItemRef_FullName']; ?></td>
                                 <td><?php echo $purchaseorderlineret_table_data_array[$y]['Description']; ?></td>
                                 <td><?php echo number_format($purchaseorderlineret_table_data_array[$y]['Quantity'], 0); ?></td>
-                                <td><?php echo $purchaseorder_table_data_array[$x]['TimeCreated']; ?></td>
-                                <td><?php echo $purchaseorder_table_data_array[$x]['Memo']; ?></td>
+
+                                <?php
+                                for ($x = 0; count($purchaseorder_table_data_array) > $x; $x++) {
+                                    if ($purchaseorder_table_data_array[$x]['TxnID'] == $purchaseorderlineret_table_data_array[$y]['PARENT_IDKEY']) : ?>
+                                        <td><?php echo $purchaseorder_table_data_array[$x]['TimeCreated']; ?></td>
+                                        <td><?php echo $purchaseorder_table_data_array[$x]['Memo']; ?></td>
+                                <?php endif;
+                                }
+                                ?>
                             </tr>
                         <?php endif; ?>
                     <?php endfor; ?>
