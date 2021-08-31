544f024c50bc7825a19be61a68f9736406d38f96
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-08-31 13:50:10 -0600

    on order list request

--- wp-content/plugins/kanbanotron/components/load_on_order_list.php
+++ wp-content/plugins/kanbanotron/components/load_on_order_list.php
@@ -40,7 +40,7 @@ for ($i = 0; count($purchaseorder_table_data_array) > $i; $i++) {
 
 ksort($order_txnid_array);
 
-echo var_dump($order_txnid_array);
+// echo var_dump($order_txnid_array);
 
 ?>
 
@@ -52,7 +52,30 @@ echo var_dump($order_txnid_array);
                 <?php echo $key; ?>
             </h5>
         </div>
-        <table class="tablesorter" id="on-order-parts-container"></table>
+        <table class="tablesorter" id="on-order-parts-container">
+            <thead>
+                <tr>
+                    <th>PN</th>
+                    <th>Description</th>
+                    <th>QTY</th>
+                    <th>Date Ordered</th>
+                    <th>PO</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($value as $ind_purchase_order) : ?>
+                    <?php foreach ($purchaseorderlineret_table_data_array as $ind_products) : ?>
+                        <?php if ($ind_purchase_order == $ind_products['PARENT_IDKEY']) : ?>
+                            <tr>
+                                <td class="full-name"><?php echo $ind_products['ItemRef_FullName']; ?></td>
+                                <td class="description"><?php echo $ind_products['Description']; ?></td>
+                                <td class="quantity"><?php echo number_format($ind_products['Quantity'], 0); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     </div>
     <!-- /Generated PO Table -->
 <?php endforeach; ?>
