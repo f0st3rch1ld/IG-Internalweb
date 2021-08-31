600c1d4918d9930fcf4b877243924dab3909013c
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-08-31 17:18:10 -0600

    on-order-list organization

--- wp-content/plugins/kanbanotron/db/wp_db/update_external_url.php
+++ wp-content/plugins/kanbanotron/db/wp_db/update_external_url.php
@@ -32,7 +32,7 @@ if ($knbn_does_url_exist_result->num_rows > 0) {
     $select_max_meta_result = $conn->query($select_max_meta_id);
     $new_max_meta;
 
-    // echo print_r($select_max_meta_result);
+    echo var_dump($select_max_meta_result->fetch_assoc());
     echo 'max_meta: ' . $new_max_meta;
     
     while ($row = $select_max_meta_result->fetch_assoc()) {
