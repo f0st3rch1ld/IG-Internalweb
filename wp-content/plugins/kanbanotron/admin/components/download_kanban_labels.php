f6b8669ef2d0e77285f4cb63c96ec763c323141b
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-09-01 13:34:30 -0600

    qrcode.js

--- wp-content/plugins/kanbanotron/admin/components/download_kanban_labels.php
+++ wp-content/plugins/kanbanotron/admin/components/download_kanban_labels.php
@@ -36,7 +36,7 @@ echo var_dump($knbn_uid_to_dwnld);
 
 <div class="knbn-lbl-grid-container">
     <div class="knbn-lbl">
-        <div id="1l234978asgfd7olua34ifrlkagsdlcv7-qrcode" style="width:100px; height:100px; margin-top:15px;" onload="makeCode('1l234978asgfd7olua34ifrlkagsdlcv7')"></div>
+        <div id="1l234978asgfd7olua34ifrlkagsdlcv7-qrcode" style="width:100px; height:100px; margin-top:15px;" onload="setTimeout(makeCode('1l234978asgfd7olua34ifrlkagsdlcv7'), 1000)"></div>
     </div>
 </div>
 
