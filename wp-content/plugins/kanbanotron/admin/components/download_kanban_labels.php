23a2f1f7d0dba08909e453021e9479d175d67352
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-09-01 14:08:54 -0600

    qrcode.js

--- wp-content/plugins/kanbanotron/admin/components/download_kanban_labels.php
+++ wp-content/plugins/kanbanotron/admin/components/download_kanban_labels.php
@@ -42,11 +42,11 @@ echo var_dump($knbn_uid_to_dwnld);
 
 <script>
     let generateCode = (x, y) => {
-        let qrcode = new QRCode(document.getElementById(`${x}-qrcode`), {
+        let QRcode = new QRCode(document.getElementById(`${x}-qrcode`), {
             width: 100,
             height: 100,
         });
-        console.log(qrcode);
+        console.log(QRcode);
         qrcode.makeCode(y.value);
         console.log(y);
     }
