ac1b82426f965b919be86e2b9deee1e9fb879262
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-09-01 11:07:50 -0600

    kanbanotron Final Pushes

--- wp-content/plugins/kanbanotron/admin/components/download_kanban_labels.php
+++ wp-content/plugins/kanbanotron/admin/components/download_kanban_labels.php
@@ -12,3 +12,48 @@ foreach ($post_ids as $post_id) {
 }
 
 echo var_dump($knbn_uid_to_dwnld);
+
+?>
+
+<style>
+    .knbn-lbl-grid-container {
+        display: grid;
+        grid-template-columns: auto auto auto auto auto;
+        justify-content: space-evenly;
+        align-content: center;
+    }
+
+    .knbn-lbl {
+        background-color: cyan;
+        width: 100px;
+        height: 100px;
+    }
+</style>
+
+<div class="knbn-lbl-grid-container">
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+    <div class="knbn-lbl"></div>
+</div>
