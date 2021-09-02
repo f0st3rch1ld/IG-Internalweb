cd342759f613c6996bd6404fae15ab67f0b37139
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-09-02 15:49:14 -0600

    adding loading screen so people dont freak out

--- wp-content/plugins/kanbanotron/admin/components/download_kanban_labels.php
+++ wp-content/plugins/kanbanotron/admin/components/download_kanban_labels.php
@@ -174,7 +174,7 @@ include plugin_dir_path(__FILE__) . '../../db/request.php';
 
                 setTimeout(function() {
                     window.location.replace("<?php echo admin_url() . 'edit.php?post_type=knbn_action&bulk_download_kanban_labels=' . count($knbn_uid_to_dwnld); ?>");
-                }, 5000);
+                }, 10000);
             }, allKnbns.length * 150);
         }
         initDownload();
