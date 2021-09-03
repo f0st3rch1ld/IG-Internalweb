a5e43268a56254ab240b504aba6d559e9bc5d0ab
Andrew Foster <62705776+f0st3rch1ld@users.noreply.github.com>
2021-09-03 07:48:25 -0600

    url update fix

--- wp-content/plugins/kanbanotron/db/wp_db/update_external_url.php
+++ wp-content/plugins/kanbanotron/db/wp_db/update_external_url.php
@@ -21,27 +21,13 @@ $knbn_does_url_exist_result = $conn->query($knbn_does_url_exist);
 
 if ($knbn_does_url_exist_result->num_rows > 0) {
     $knbn_set_ext_url = "UPDATE wp_postmeta SET meta_value='$ext_url' WHERE meta_key='external_product_url' AND post_id='$retrieved_id'";
-
     if ($conn->query($knbn_set_ext_url) === TRUE) {
         echo "External URL updated";
     } else {
         echo "Error updating external URL: " . $conn->error;
     }
 } else {
-    $select_max_meta_id = "SELECT meta_id FROM wp_postmeta";
-    $select_max_meta_result = $conn->query($select_max_meta_id);
-    $max_meta_array = array();
-
-    while ($row = $select_max_meta_result->fetch_assoc()) {
-        $max_meta_array = [$row['meta_id']];
-    }
-
-    echo var_dump(max($max_meta_array));
-    $new_max_meta = max($max_meta_array);
-    $new_max_meta++;
-    echo $new_max_meta;
-
-    $knbn_set_ext_url = "INSERT INTO wp_postmeta (meta_id, post_id, meta_key, meta_value) VALUES ($new_max_meta, $retrieved_id, 'external_product_url', " . json_encode($ext_url) . ")";
+    $knbn_set_ext_url = "INSERT INTO wp_postmeta (post_id, meta_key, meta_value) VALUES ($retrieved_id, 'external_product_url', " . json_encode($ext_url) . ")";
 
     if ($conn->query($knbn_set_ext_url) === TRUE) {
         echo "External URL updated";
